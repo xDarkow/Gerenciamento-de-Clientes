64e79ae871a6333d858698d3d782f7eb2b93d5b4
Darkow <felipeanderson_@hotmail.com>
2021-04-09 15:25:29 +0000

    folder para arquivos de função e formatação da parte de cadastro

--- main.c
+++ main.c
@@ -3,19 +3,21 @@
 #include <ctype.h>
 #include <time.h>
 #include <string.h>
-#include "funcoes.h"
-#include "validacao.h"
+#include "functions/funcoes.h"
+#include "functions/validacao.h"
 
 typedef struct dados Dados;
 
 struct dados{
   char cpf[15];
-  char name[51]; // fazer de forma dinâmica
+  char name[51];
   char bday[11];
-  char adress[101]; //fazer de forma dinâmica
+  char adress[101];
+  char cidade[31];
+  char uf[3];
   char phone[15];
   char sexo[10];
-  char email[51]; //fazer de forma dinâmica
+  char email[51];
 };
 
 struct vendas{
@@ -30,8 +32,8 @@ void clienteinfo(const Dados*);
 int main(void) {
   Dados* cliente;
   int opt;
-  opt = mainmenu();
-  while (opt != 0){
+  do{
+    opt = mainmenu();
     switch (opt){
       case 1: 
         cliente = cadastro();
@@ -47,6 +49,7 @@ int main(void) {
         printf("\n\nPrecione alguma tecla para sair do programa");
         getchar();
         getchar();
+        system("clear");
         break;
       default:
         system("clear");
@@ -54,44 +57,50 @@ int main(void) {
         printf("\n\nPrecione alguma tecla para voltar ao menu principal");
         getchar();
         getchar();
+        break;
     }
-    opt = mainmenu();
-  }
+  }while(opt != 0);
   free(cliente);
   return 0;
 }
 
 Dados* cadastro(){
   system("clear");
-  printf("=== Cadastro de clientes ===\n\n");
+  printf("=== Cadastro de clientes ===");
+  printf("\n\n");
+  printf("Por favor informe os dados dos clientes a seguir!\n\n");
   Dados* dds;
   dds = (Dados*) malloc(sizeof(Dados));
-  printf("Informe o cpf do cliente:                ");
+  printf("CPF:                       ");
   scanf(" %14[^\n]", dds->cpf);
   //onlynumb(dds->cpf);
   while(validacpf(dds->cpf)!=1){
     // system("clear");
     printf("\nVocê não digitou um CPF válido!\n");
-    printf("Informe o cpf do cliente:                ");
+    printf("CPF:                       ");
     scanf(" %14[^\n]", dds->cpf);
     //onlynumb(dds->cpf);
   }
-  printf("Informe o nome do cliente:               ");
+  printf("NOME:                      ");
   scanf(" %50[^\n]", dds->name);
   while(validaname(dds->name)!=1){
     printf("\nVocê não digitou um Nome válido!\n");
-    printf("Informe o nome do cliente:               ");
+    printf("NOME:                      ");
     scanf(" %50[^\n]", dds->name);
   }
-  printf("Informe a data de nascimento do cliente: ");
+  printf("DATA DE NASCIMENTO:        ");
   scanf(" %10[^\n]", dds->bday);
-  printf("Informe o endereço do cliente:           ");
+  printf("UF(Unidade Federativa):    ");
+  scanf(" %2[^\n]", dds->uf);
+  printf("CIDADE:                    ");
+  scanf(" %30[^\n]", dds->cidade);
+  printf("ENDEREÇO:                  ");
   scanf(" %100[^\n]", dds->adress);
-  printf("Informe o telefone/celular do cliente:   ");
+  printf("TELEFONE/CELULAR:          ");
   scanf(" %14[^\n]", dds->phone);
-  printf("Informe o sexo do cliente:               ");
+  printf("SEXO (Masculino/Feminino): ");
   scanf(" %9[^\n]", dds->sexo);
-  printf("Informe o email do cliente:              ");
+  printf("EMAIL:                     ");
   scanf(" %50[^\n]", dds->email);
   return dds;
 }
